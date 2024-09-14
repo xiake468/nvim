5c55b539b8a1b05f82725cd3de21ae3ac9306d7d
maliang307694 <ma77045728@gmail.com>
2024-09-14 17:13:34 +0800

    java not complete

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.number = true -- 显示行号
 vim.opt.cursorline = true -- 高亮所在行
 vim.o.showmode = false -- 关闭vim自带的visual模式
 -- tab
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,19 +3,48 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
             require('mason').setup()
-		end
+		end,
+        opts = {
+			registries = {
+				"github:nvim-java/mason-registry",
+				"github:mason-org/mason-registry",
+			},
+		},
    },
    {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 		    require('mason-lspconfig').setup {
-				 ensure_installed = { 'clangd' },
+				 ensure_installed = { 'clangd', 'jdtls' },
 			}
 		end
    },
    {
+	    'nvim-java/nvim-java',
+	    dependencies = {
+		    'nvim-java/lua-async-await',
+		    'nvim-java/nvim-java-refactor',
+		    'nvim-java/nvim-java-core',
+		    'nvim-java/nvim-java-test',
+		    'nvim-java/nvim-java-dap',
+		    'MunifTanjim/nui.nvim',
+		    'neovim/nvim-lspconfig',
+		    'mfussenegger/nvim-dap',
+		    {
+			    'JavaHello/spring-boot.nvim',
+			    commit = '218c0c26c14d99feca778e4d13f5ec3e8b1b60f0',
+		    },
+	    },
+    },
+    {
 		"neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+			"folke/neodev.nvim",
+        },
 		config = function()
+            require('java').setup({})
+            require('lspconfig').jdtls.setup{}
 		    require('lspconfig')['clangd'].setup{}
 		end
    },
